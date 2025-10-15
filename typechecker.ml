f9fe9cc62ad36c0593469bf3c9f53b91341cab89
laorine.lagarde <laorine.lagarde@universite-paris-saclat.fr>
2025-10-15 11:43:13 +0200

    defini mini iast

--- typechecker.ml
+++ typechecker.ml
@@ -1,1 +1,9 @@
 open Types
+
+type Iprogram = (string*iAST list) list * (string*int) list
+
+and iAST = | Ireturn of expr | Ival of expr
+
+and value =  | Iconst of int
+
+and expr = | Ivalue of value
