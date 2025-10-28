662a8a65736ec2178cf362fa466bb435ae799ee0
laorine.lagarde <laorine.lagarde@universite-paris-saclat.fr>
2025-10-28 12:51:07 +0100

    petit retour en arriere car trop complique a gerer avec les Ilocal

--- lib/conversion.ml
+++ lib/conversion.ml
@@ -1,52 +1,40 @@
 open AST1
 open AST2
 
-(*Pour les Ilocal*)
-let temp_counter = ref 0
-let new_temp () =
-  incr temp_counter;
-  !temp_counter
-
-let rec expr1_to_iAST (e : expr) : (iAST list * value) = 
-(*La iAST list correspond aux instructions intermédiaires générées*)
-(*Dans value se trouve le resultat final*)
+let rec expr_to_iexpr (e : expr) : iexpr = 
    match e with
-  | Cst (n, _) ->([], Iconst n)
-
+  | Cst (n, _) -> Ivalue (Iconst n)
   | Binop (op, e1, e2, _) ->
-      let (code1, v1) = expr1_to_iAST e1 in
-      let (code2, v2) = expr1_to_iAST e2 in
-      let tmp = new_temp () in
-      let lv = (Ilocal tmp, 8) in
-      let assign = Iassign (lv, Ibinop (op, v1, v2)) in
-      (code1 @ code2 @ [assign], Ileft lv)
+      let v1 = expr_to_iexpr e1 in
+      let v2 = expr_to_iexpr e2 in
+      (match (v1, v2) with
+       | (Ivalue val1, Ivalue val2) -> Ibinop (op, val1, val2)
+       | _ -> failwith "binop pas implemente")
 
   | Unop (op, e1, _) ->
-      let (code1, v1) = expr1_to_iAST e1 in
-      let tmp = new_temp () in
-      let lv = (Ilocal tmp, 8) in
-      let assign = Iassign (lv, Iunop (op, v1)) in
-      (code1 @ [assign], Ileft lv)
+      let v1 = expr_to_iexpr e1 in
+      (match v1 with
+       | Ivalue val_ -> Iunop (op, val_)
+       | _ -> failwith "unop pas implemente")
 
-  | _ -> failwith " expression non supporte"
+  | _ -> failwith " expression pas implemente"
 
   (*renvoie une liste de iAST*)
-let stmt1_to_iAST (s : stmt) : iAST list=
+let stmt_to_iAST (s : stmt) : iAST list=
   match s with
-  | Print (e, _) -> let (code, v) = expr1_to_iAST e in
-    code @ [ Iassign ((Ilocal (new_temp ()), 8), Ivalue (Ileft (Iglobal "fmt", 32)));
-          Iassign ((Ilocal (new_temp ()), 8), Ivalue v);
-          Icall "printf"; ]
+  | Print (e, _) -> let v = expr_to_iexpr e in
+      [ Iassign ((Ireg "rdi", 8), v); 
+        Icall "printf" ] 
 
-  | Return (e, _) ->let (code, v) = expr1_to_iAST e in
-      code @ [Ireturn (Ivalue v)]
+  | Return (e, _) -> let v = expr_to_iexpr e in
+      [ Ireturn (Ivalue v) ]
 
   | _ -> failwith "stmt non géré"
 
 let gdef1_to_iAST_list (g : gdef) : (string * iAST list) =
   match g with
   | Function (name, _arg, stmts, _) ->
-      (name, List.flatten (List.map stmt1_to_iAST stmts)) (*On concatene les differentes listes de iAST*)
+      (name, List.flatten (List.map stmt_to_iAST stmts)) (*On concatene les differentes listes de iAST*)
   | _ -> failwith "expression pas encore pris en compte"
 
 let program1_to_iprogram (p : program) : iprogram =
