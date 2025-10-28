4167ec82d4bb0eb31d6536aae6065b305674b2c3
evairson <eva.herson@me.com>
2025-10-28 09:01:55 +0100

    changement production

--- lib/production.ml
+++ lib/production.ml
@@ -9,18 +9,11 @@ let compile_pos (p : pos) : string =
 let compile_ivalue (v : value) : string =
   match v with 
   | Iconst n -> 
-      Printf.sprintf "   mov $%d, %%eax\n" n
+      Printf.sprintf "   push $%d\n" n
   | Ileft (lv, _) ->
       (match lv with
-       | Ilocal i -> Printf.sprintf " ??"
-       | Iglobal s -> Printf.sprintf "??" s
-       | _ -> failwith "pas traite")
-  (*Maintenant Iglobal est une pos *)
-  (*
-  | Iglobal s -> 
-      Printf.sprintf "   lea %s(%%rip), %%rax\n" s
-  | Ireg r -> 
-      Printf.sprintf "   mov %%%s, %%rax\n" r*)
+       | Ilocal i -> Printf.sprintf "   mov -%d(%%rbp), %%rax\n   push %%rax\n" (8 * i)
+       | Iglobal s -> Printf.sprintf  "   mov %s(%%rip), %%rax\n   push %%rax\n" s)
   
 let compile_expr (e : iexpr) : string =
   match e with 
@@ -28,54 +21,56 @@ let compile_expr (e : iexpr) : string =
   | Iunop (op, v) ->
       let v_code = compile_ivalue v in
       begin match op with
-      | Opp -> "??"
-      | _ -> failwith "Operation unaire pas encore implementee"
+      | Opp -> v_code ^ "   pop %%rax\n   neg %%rax\n   push %%rax\n"
+      | Not -> v_code ^ "   pop %%rax\n   not %%rax\n   push %%rax\n"
       end
   | Ibinop (op, v1, v2) ->
-      let v1_code = compile_ivalue v1 in
-      let v2_code = compile_ivalue v2 in
+      let _v1_code = compile_ivalue v1 in
+      let _v2_code = compile_ivalue v2 in
       begin match op with
-      | Plus -> "??"
-      | Minus -> "??"
-      | Mul -> "??"
-      | Div -> "??"
-      | Rem -> "??"
+      | Plus -> "   pop %%rbx\n   pop %%rax\n   add %%rbx, %%rax\n   push %%rax\n"
+      | Minus -> "    pop %%rbx\n   pop %%rax\n   sub %%rbx, %%rax\n   push %%rax\n"
+      | Mul -> "    pop %%rbx\n   pop %%rax\n   imul %%rbx, %%rax\n   push %%rax\n"
+      | Div -> "    pop %%rbx\n   pop %%rax\n   xor %%rdx, %%rdx\n   idiv %%rbx\n   push %%rax\n"
+      | Rem -> "    pop %%rbx\n   pop %%rax\n   xor %%rdx, %%rdx\n   idiv %%rbx\n   push %%rdx\n"
       | _ -> failwith "Operation binaire pas encore implementee"
       end
-  | _ -> failwith "Expression pas encore implementee"
 
-
-let compile_ast (s : iAST) : string =
-  match s with 
+let compile_ast (ast : iAST) : string =
+  match ast with 
   | Ireturn e -> 
       (let expr_code = compile_expr e in
       expr_code ^ "   ret\n")
+
   | Ival e -> 
       compile_expr e
-  (* A modifier. Maintenanr Iassign ((pos, size), iexpr) pos est un int generer pour chaque nouvelle allocation. Je ne sais pas trop comment convertir ca en var*)
+  (* A modifier. Maintenant Iassign ((pos, size), iexpr) pos est un int generer pour chaque nouvelle allocation. Je ne sais pas trop comment convertir ca en var*)
   (*| Iassign (var, e, size) ->
       (let expr_code = compile_expr e in
       match size with
       | 32 -> expr_code ^ Printf.sprintf "   mov %%eax, %, %%%s\n" var
       | _ -> expr_code ^ Printf.sprintf "   mov %%rax, %, %%%s\n" var)*)
+
   | Iassign ((pos, size), e) ->
       let expr_code = compile_expr e in
-      match size with
+      (match size with
       | 32 -> expr_code ^ Printf.sprintf "   mov %%eax, %s\n" (compile_pos pos)
-      | _ -> expr_code ^ Printf.sprintf "   mov %%rax, %s\n" (compile_pos pos)
+      | _ -> expr_code ^ Printf.sprintf "   mov %%rax, %s\n" (compile_pos pos))
 
   | Icall s ->
-      Printf.sprintf " xor %%eax, %%eax \n   call %s\n" s
+      Printf.sprintf "   xor %%eax, %%eax \n   call %s\n" s
+
 
 let compile_asts (name : string) (asts : iAST list) : string =
   let header = Printf.sprintf ".global %s \n %s:\n" name name in
   let body = List.fold_left (fun acc ast -> acc ^ (compile_ast ast)) "" asts in
   header ^ body
 
-let compile_program (prg : iprogram) file =
+
+let compile_program (prog : iprogram) file =
   let oc = open_out file in
   let print oc s = output_string oc (s ^ "\n") in
-  let (cmd, _vars) = prg in
+  let (cmd, _vars) = prog in
 
   print oc ".extern printf";
   print oc ".section .data";
